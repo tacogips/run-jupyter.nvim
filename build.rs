1d7f3548981dade5dae6956f80d64a9171f3b464
tacogips <me@tacogips.me>
2022-05-24 14:43:51 +0900

    some

--- build.rs
+++ build.rs
@@ -1,15 +1,12 @@
 use std::path::{Path, PathBuf};
 
 fn main() {
+    build_python();
+    build_julia();
+    build_rust();
+}
+fn build_python() {
     let src_dir: PathBuf = ["treesitter", "tree-sitter-python", "src"].iter().collect();
-
-    //cc::Build::new()
-    //    .include(&dir)
-    //    .file(dir.join("parser.c"))
-    //    .file(dir.join("scanner.cc"))
-    //    .compile("tree-sitter-python");
-
-    //let src_dir = Path::new("src");
 
     let mut c_config = cc::Build::new();
     c_config.include(&src_dir);
@@ -32,4 +29,23 @@ fn main() {
     cpp_config.file(&scanner_path);
     println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
     cpp_config.compile("scanner");
+}
+
+fn build_julia() {}
+
+fn build_rust() {
+    let src_dir: PathBuf = ["treesitter", "tree-sitter-rust", "src"].iter().collect();
+    let mut config = cc::Build::new();
+    config.include(&src_dir);
+    config
+        .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wno-unused-but-set-variable")
+        .flag_if_supported("-Wno-trigraphs");
+    let parser_path = src_dir.join("parser.c");
+    let scanner_path = src_dir.join("scanner.c");
+    println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
+    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
+    config.file(&parser_path);
+    config.file(&scanner_path);
+    config.compile("parser-scanner");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use tree_sitter::{Language, Parser};
 extern "C" {
     fn tree_sitter_python() -> Language;
+    fn tree_sitter_julia() -> Language;
+    fn tree_sitter_rust() -> Language;
 }
 
 fn main() {
-    let lang = unsafe { tree_sitter_python() };
+    let python_lang = unsafe { tree_sitter_python() };
+    //let julia_lang = unsafe { tree_sitter_julia() };
+    let rust_lang = unsafe { tree_sitter_rust() };
     let mut parser = Parser::new();
-    parser.set_language(lang).unwrap();
+    //parser.set_language(python_lang).unwrap();
+    //parser.set_language(python_lang).unwrap();
+    ////parser.set_language(julia_lang).unwrap();
+    //parser.set_language(rust_lang).unwrap();
     let code = r#"
 
 ### aa
@@ -26,6 +33,9 @@ def some(a):
     //println!("{:?}", root_node.child(0).unwrap().kind());
     //println!("{:?}", root_node.utf8_text(code.as_bytes()));
     let mut cursor = root_node.walk();
+
+    println!("{:?}", root_node);
+
     println!("{:?}", cursor.field_name());
     //while let Some(a) = root_node.walk() {
     //    println!("{:?}", a);
