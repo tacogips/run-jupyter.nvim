121431a1654eddfe2a0f0c7bf74f6de44f5d5ba8
tacogips <me@tacogips.me>
2022-05-24 14:07:20 +0900

    some

--- build.rs
+++ build.rs
@@ -1,9 +1,35 @@
-fn main() {
-    let dir: PathBuf = ["treesitter", "tree-sitter-python", "src"].iter().collect();
+use std::path::{Path, PathBuf};
 
-    cc::Build::new()
-        .include(&dir)
-        .file(dir.join("parser.c"))
-        .file(dir.join("scanner.c"))
-        .compile("tree-sitter-python");
+fn main() {
+    let src_dir: PathBuf = ["treesitter", "tree-sitter-python", "src"].iter().collect();
+
+    //cc::Build::new()
+    //    .include(&dir)
+    //    .file(dir.join("parser.c"))
+    //    .file(dir.join("scanner.cc"))
+    //    .compile("tree-sitter-python");
+
+    //let src_dir = Path::new("src");
+
+    let mut c_config = cc::Build::new();
+    c_config.include(&src_dir);
+    c_config
+        .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wno-unused-but-set-variable")
+        .flag_if_supported("-Wno-trigraphs");
+    let parser_path = src_dir.join("parser.c");
+    c_config.file(&parser_path);
+    println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
+    c_config.compile("parser");
+
+    let mut cpp_config = cc::Build::new();
+    cpp_config.cpp(true);
+    cpp_config.include(&src_dir);
+    cpp_config
+        .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wno-unused-but-set-variable");
+    let scanner_path = src_dir.join("scanner.cc");
+    cpp_config.file(&scanner_path);
+    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
+    cpp_config.compile("scanner");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use tree_sitter::{Language, Parser};
+extern "C" {
+    fn tree_sitter_python() -> Language;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lang = unsafe { tree_sitter_python() };
+    let mut parser = Parser::new();
+    parser.set_language(lang).unwrap();
+    let code = r#"
+
+### aa
+def some(a):
+    """ sss """
+    print("----",a)
+
+    "#;
+
+    let tree = parser.parse(code, None).unwrap();
+
+    //println!("{:?}", tree);
+    let root_node = tree.root_node();
+    //println!("{:?}", root_node.to_sexp());
+    //println!("{:?}", root_node.start_position());
+
+    //println!("{:?}", root_node.child(0).unwrap().kind());
+    //println!("{:?}", root_node.utf8_text(code.as_bytes()));
+    let mut cursor = root_node.walk();
+    println!("{:?}", cursor.field_name());
+    //while let Some(a) = root_node.walk() {
+    //    println!("{:?}", a);
+    //}
 }
