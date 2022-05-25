fd51646a6db8e9b1cb44ca3d05379e084a068ac2
tacogips <me@tacogips.me>
2022-05-25 23:36:56 +0900

    some

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	cargo build --release
+	cp ./target/release/librun_jupyter.so lua/librun-jupyter.so
+
+build-dev:
+	cargo build
+	cp ./target/debug/librun_jupyter.so lua/librun-jupyter.so
+
---
+++ src/lua_entrypoint.rs
@@ -0,0 +1,37 @@
+use mlua::prelude::*;
+
+async fn start_kernel(
+    _lua: &Lua,
+    (jupyter_url, kernel_name): (String, String),
+) -> LuaResult<String> {
+    //let result = Runtime::new().unwrap().block_on(build_client_and_request(
+    //    &cmd_name,
+    //    server::default_socket_path(),
+    //    input,
+    //));
+    //match result {
+    //    Ok(result) => Ok(result.output),
+    //    Err(e) => Ok(format!("error:{}", e)),
+    //}
+    unimplemented!()
+}
+
+async fn list_kernels(_lua: &Lua, jupyter_url: String) -> LuaResult<Vec<String>> {
+    unimplemented!()
+}
+
+async fn send_code(
+    _lua: &Lua,
+    (jupyter_url, kernel, code): (String, String, String),
+) -> LuaResult<LuaTable<'_>> {
+    unimplemented!()
+}
+
+#[mlua::lua_module]
+fn run_jupyter(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    exports.set("start_kernel", lua.create_async_function(start_kernel)?)?;
+    exports.set("list_kernels", lua.create_async_function(list_kernels)?)?;
+    exports.set("send_code", lua.create_async_function(send_code)?)?;
+    Ok(exports)
+}
